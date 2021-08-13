bedb179bf07fa116c88a7bcdfba9721bae47fd31
Hariharan-710 <hariharan98710@gmail.com>
2021-08-13 22:28:54 +0530

    Commit

--- src/Header.js
+++ src/Header.js
@@ -8,7 +8,7 @@ function Header() {
             <svg xmlns="http://www.w3.org/2000/svg" class="10-9 w-10" fill="none" viewBox="0 0 24 24" stroke="white">
   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 8h6m-5 0a3 3 0 110 6H9l3 3m-3-6h6m6 1a9 9 0 11-18 0 9 9 0 0118 0z" />
 </svg>    
-            <h1 className=" text-3xl md:text-4xl text-center text-gray-100 font-semibold   ">Currency Convertor</h1> 
+            <h1 className=" text-3xl md:text-4xl text-center text-gray-100 font-semibold   ">Currency Converter</h1> 
             </div>
             
         
